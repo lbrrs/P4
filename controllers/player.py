7923116f51850cd333c375da3dc09163712f2cb6
lbrrs <barroislucas@outlook.fr>
2022-10-23 15:47:01 +0200

    oighnze<

--- controllers/player.py
+++ controllers/player.py
@@ -1,6 +1,6 @@
 # from controllers.menu import MenuController
 import re
-
+from datetime import datetime
 
 class Valid:
 
@@ -9,11 +9,10 @@ class Valid:
         pass
 
     #Name
-    def is_valid_name(name):
+    def is_valid_name(self, name):
         # regex
         pattern = '^[a-zA-Z]+$'
-        re.match(pattern, name)
-        return name.is_valid_name(name)
+        return re.match(pattern, name)
 
 
     def get_name(self):
@@ -25,11 +24,10 @@ class Valid:
 
 
     #Firstname
-    def is_valid_firstname(firstname):
+    def is_valid_firstname(self, firstname):
         # regex
         pattern = '^[a-zA-Z]+$'
-        re.match(pattern, firstname)
-        return firstname.is_valid_name(firstname)
+        return re.match(pattern, firstname)
 
 
     def get_firstname(self):
@@ -41,30 +39,31 @@ class Valid:
 
 
     #Date
-    def is_valid_date(date):
+    def is_valid_date(self, date):
         # regex
-        pattern = '^[0-9]+$'
-        re.match(pattern, date)
-        return date.is_valid_date(date)
+        try:
+            pattern = datetime.strptime(date, '%m-%d-%Y').date()
+            return True
+        except ValueError:
+            return False
+
 
 
     def get_date(self):
         date = input("date :")
-        if self.is_valid_name(date):
+        if self.is_valid_date(date):
             return date
         print('Invalid date')
         return self.get_date()
 
     #Sex
-    def is_valid_sex(sex):
+    def is_valid_sex(self, sex):
         # regex
         pattern = '^[a-zA-Z]+$'
-        re.match(pattern, sex)
-        return sex.is_valid_sex(sex)
+        return re.match(pattern, sex)
 
-
-    def get_sex(self, sex=None):
-        name = input("sex :")
+    def get_sex(self):
+        sex = input("sex :")
         if self.is_valid_sex(sex):
             return sex
         print('Invalid sex')
@@ -73,12 +72,11 @@ class Valid:
     # rank
     def is_valid_rank(self, rank):
         # regex
-        pattern = '^[a-zA-Z]+$'
-        re.match(pattern, rank)
-        return self.is_valid_rank(rank)
+        pattern = '[0-9]{4}'
+        return re.match(pattern, rank)
 
-    def get_rank(self, rank):
-        name = input("rank :")
+    def get_rank(self):
+        rank = input("rank :")
         if self.is_valid_rank(rank):
             return rank
         print('Invalid rank')
--- vues/menu.py
+++ vues/menu.py
@@ -1,7 +1,11 @@
+import re
+from model.player import Player
+
 class Views:
 
     def __init__(self):
-        pass
+        self.player = Player()
+
 
     @staticmethod
     def display_menu(menu):
@@ -13,7 +17,6 @@ class Views:
         print('Choix incorrect')
         return Views.display_menu(menu)
 
-
     @staticmethod
     def display_menubis(menu):
         for index, choice in menu:
@@ -24,4 +27,58 @@ class Views:
         print('Choix incorrect')
         return Views.display_menubis(menu)
 
+    @staticmethod
+    def display_player(lst_player):
+        for count, player in enumerate(lst_player, 1):
+            print(f'{count}: {player}')
+        reponse = input('Choix : ')
+        if int(reponse) <= len(lst_player):
+            print(lst_player[int(reponse)-1])
+        else:
+            print('Choix incorrect')
+        return lst_player[int(reponse)-1]
 
+    @staticmethod
+    def display_tournament(menu, lst_player):
+        for index, choice in menu:
+            print(index,')', choice)
+        reponse = input('Choix : ')
+        if reponse in menu:
+            return reponse
+        print('Choix incorrect')
+        return Views.display_menubis(menu)
+
+    @staticmethod
+    def display_tournaments(menu):
+        for index, choice in menu:
+            print(index,')', choice)
+        reponse = input('Choix : ')
+        if reponse in menu:
+            return reponse
+        print('Choix incorrect')
+
+    @staticmethod
+    def display_players_tournament(lst_player):
+        lst_plr_t = set()
+        lst_ser = list()
+        for i in lst_player:
+            lst_ser.append(Player.deserialize_player(lst_player[i]))
+
+
+        for i in lst_player:
+            lst_plr_t.add(i)
+        while len(lst_plr_t) >= 8:
+            for count, player in enumerate(lst_player, 1):
+                print(f'{count}: {player}')
+            len_prev = len(lst_plr_t)
+            pattern = '[0-9]{1}'
+            pattern1 = '[0-9]{2}'
+            reponse = input('Choix : ')
+            if re.match(pattern, reponse) is not None or re.match(pattern1, reponse) is not None:
+                lst_plr_t.add(reponse)
+
+            if len(lst_plr_t) > len_prev:
+                print(lst_plr_t)
+            else:
+                print('Choix incorrect')
+        return lst_plr_t
